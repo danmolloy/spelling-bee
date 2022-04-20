7a4ba60af4236c1bd510e3e9755d72cd3c56c9d6
Daniel Molloy <danielmolloy_6@icloud.com>
2022-04-20 15:05:05 +0100

    css adjustment

--- components/letters.js
+++ components/letters.js
@@ -6,15 +6,15 @@ export default function Letters({setLetter, shuffle, shuffledLetters, data}) {
   
 
   return (
-    <div className="w-full letters justify-center flex flex-row p-6">
+    <div className="w-full letters justify-center flex flex-row md:p-6">
       
       <div className="left flex flex-col h-full ">
         <Hexagon letter={shuffledLetters && shuffledLetters[0]} setLetter={() => setLetter(shuffledLetters[0])}/>
         <Hexagon letter={shuffledLetters && shuffledLetters[1]} setLetter={() => setLetter(shuffledLetters[1])}/>
       </div>
-      <div className=" flex flex-col h-full -mt-14">
+      <div className=" flex flex-col h-full -mt-10 md:-mt-14">
         <Hexagon letter={shuffledLetters && shuffledLetters[2]} setLetter={() => setLetter(shuffledLetters[2])}/>
-        <Hexagon data-testid="center-letter" center letter={data && data.centerLetter.toUpperCase()} setLetter={() => setLetter(data.centerLetter.toUpperCase())}/>
+        <Hexagon data-testid="center-letter" center letter={/* data && data.centerLetter.toUpperCase()} setLetter={() => setLetter(data.centerLetter.toUpperCase()) */""}/>
         <Hexagon letter={shuffledLetters && shuffledLetters[4]} setLetter={() => setLetter(shuffledLetters[4])}/>
       </div>
       <div className="flex flex-col">
