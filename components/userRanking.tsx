0b13c6b9bd076a41eae68beb220872382b8213b0
Daniel Molloy <danielmolloy_6@icloud.com>
2022-05-08 18:09:30 +0100

    typescript added

--- components/userRanking.tsx
+++ components/userRanking.tsx
@@ -1,34 +1,26 @@
 import { rankingLevels } from "./rankings";
-import { useEffect } from 'react'
 
 export default function UserRanking({ rankIndex, currentPoints, data }) {
-
-  useEffect(() => {
-    RankingVisual()
-  }, [currentPoints])
-
-  const RankingVisual = () => rankingLevels.map(i => (
-      <div key={i.name}>
-        {rankingLevels.indexOf(i) === rankIndex 
-        ? <div className="current-rank-icon bounce">
-            <p className="font-thin text-sm ">{currentPoints}</p>
-          </div>
-        : rankingLevels.indexOf(i) < rankIndex
-        ? <div className="past-rank-icon"></div>
-        : rankingLevels.indexOf(i) === rankingLevels.length - 1
-        ? <div className="final-rank-icon"></div>
-        : <div className="future-rank-icon"></div>
-      }
-      </div>
-    ))
-
 
   return (
     <div className='flex flex-row w-full items-center justify-center'>
         <h3 className='flex items-center justify-center p-1  w-32 font-semibold'>{rankingLevels[rankIndex].name}</h3>
         <div className="w-full">
           <div className="ranking-bar">
-            <RankingVisual />
+          {rankingLevels.map(i => (
+            <div key={i.name}>
+              {rankingLevels.indexOf(i) === rankIndex 
+              ? <div className="current-rank-icon bounce">
+                  <p className="font-thin text-sm ">{currentPoints}</p>
+                </div>
+              : rankingLevels.indexOf(i) < rankIndex
+              ? <div className="past-rank-icon"></div>
+              : rankingLevels.indexOf(i) === rankingLevels.length - 1
+              ? <div className="final-rank-icon"></div>
+              : <div className="future-rank-icon"></div>
+            }
+          </div>))}
+            
           </div>
           <div className="line"></div>
         </div>
