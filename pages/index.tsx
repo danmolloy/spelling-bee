173876c38afc509c36a1a5be690b3f8f17d6f3c8
Daniel Molloy <danielmolloy_6@icloud.com>
2023-04-17 17:17:15 +0100

    getServerSideProps added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
-import useSwr from 'swr'
 import GameIndex from '../components';
 import Loading from '../components/loading';
 
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
 
-export default function Home() {
-  const { data, error } = useSwr('/api/bee', fetcher)
+export default function Home(data) {
 
   if (!data) {
     return <Loading />
@@ -16,3 +13,16 @@ export default function Home() {
     <GameIndex data={data.data.yesterday}/>
   )
 }
+ 
+export async function getServerSideProps() {
+  const data = await fetch('https://www.nytimes.com/puzzles/spelling-bee')
+            .then(res => res.text())
+            .then(data => JSON.parse(data.slice(data.indexOf("gameData") + 11, data.indexOf("}}", data.indexOf("gameData")) + 2)))
+            //.then(data => ({ data: data }))
+  //console.log(beeData)
+  return {
+    props: {
+      data
+    }, // will be passed to the page component as props
+  }
+} 
